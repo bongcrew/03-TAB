9288b16f9f2600c27537fc3904dc2cc3a394d1db
bongcrew <bongcrew1@naver.com>
2023-12-01 11:05:57 +0900

    배경이미지와 타이틀 적용

--- 02/js/script.js
+++ 02/js/script.js
@@ -10,9 +10,26 @@ $(function () {
   const $tabMenu = $('.tab-menu > li');
   const $tabContent = $('.tab-con-item');
 
-  // 배경이미지를 배열에 저장
-  const bgArr = ['blackpink01.jpg', 'blackpink02.jpg', 'blackpink03.jpg'];
-  console.log(bgArr[2]);
+  // 배열에 이미지와 텍스트 정보를 저장
+  const bgArr = [
+    {
+      pic: 'blackpink01.jpg',
+      title: '지수가 제일 예뻐요',
+    },
+    {
+      pic: 'blackpink02.jpg',
+      title: '지수가 정말 예뻐요',
+    },
+    {
+      pic: 'blackpink03.jpg',
+      title: '지수가 항상 예뻐요',
+    },
+  ];
+
+  console.log(bgArr);
+  console.log(bgArr[1]);
+  console.log(bgArr[1].title);
+  console.log(bgArr[1]['title']);
 
   // 초기화
   let idx = 0;
@@ -26,9 +43,11 @@ $(function () {
 
     // 배경이미지 적용
     $body.css({
-      // background: 'url(img/' + bgArr[index] + ') no-repeat 0 0 /cover',
-      background: `url(img/${bgArr[index]}) no-repeat 0 0 /cover`,
+      background: `url(img/${bgArr[index].pic}) no-repeat 0 0 /cover`,
     });
+
+    // 탭 컨텐츠의 제목을 수정
+    $tabContent.eq(index).find('h2').text(bgArr[index].title);
   }
 
   $tabMenu.on('click', function (e) {
